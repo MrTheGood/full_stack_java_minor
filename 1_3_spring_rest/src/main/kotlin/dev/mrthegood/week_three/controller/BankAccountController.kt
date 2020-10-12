6f35df6d2287ac3e5d596d279c70a76974d2d0a1
MrTheGood <maarten@degoedeweb.nl>
2020-10-12 22:15:53 +0200

    Remove explicit HttpStatus.OK definitions from controllers, replace HttpStatus.CREATED with ResponseEntity

--- 1_3_spring_rest/src/main/kotlin/dev/mrthegood/week_three/controller/BankAccountController.kt
+++ 1_3_spring_rest/src/main/kotlin/dev/mrthegood/week_three/controller/BankAccountController.kt
@@ -4,8 +4,10 @@ import dev.mrthegood.week_three.models.BankAccount
 import dev.mrthegood.week_three.services.BankAccountService
 import dev.mrthegood.week_three.util.exception.BankAccountNotFoundException
 import dev.mrthegood.week_three.util.exception.IllegalBankAccountException
-import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.net.URI
+
 
 @RestController
 @RequestMapping("BankAccount")
@@ -14,50 +16,46 @@ class BankAccountController(
 ) {
 
     @GetMapping("")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
     fun getBankAccounts() = bankAccountService.bankAccounts
 
+
     @GetMapping("/{id}")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
     fun getBankAccount(@PathVariable id: Long): BankAccount {
         return bankAccountService.getBankAccount(id) ?: throw BankAccountNotFoundException("Account `$id` not found")
     }
 
+
     @PostMapping("")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.CREATED)
-    fun createAccount(@RequestBody account: BankAccount): Long {
+    fun createAccount(@RequestBody account: BankAccount): ResponseEntity<Long> {
         if (account.iBAN.isBlank())
             throw IllegalBankAccountException("Account holder `${account.id}` cannot be created. IBAN cannot be blank")
         if (account.accountHolders.isEmpty())
             throw IllegalBankAccountException("Account holder `${account.id}` cannot be created. Requires at least 1 account holder")
-        return bankAccountService.createAccount(account)
+
+        val id = bankAccountService.createAccount(account)
+        return ResponseEntity.created(URI("/$id")).apply { body(id) }.build()
     }
 
+
     @PutMapping("")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
     fun updateAccount(@RequestBody account: BankAccount) {
         if (account.iBAN.isBlank())
             throw IllegalBankAccountException("Account holder `${account.id}` cannot be updated. IBAN cannot be blank")
         if (account.accountHolders.isEmpty())
             throw IllegalBankAccountException("Account holder `${account.id}` cannot be updated. Requires at least 1 account holder")
+
         bankAccountService.updateAccount(account)
     }
 
+
     @PutMapping("/block/{id}")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
     fun blockAccount(@PathVariable id: Long) {
         val account = bankAccountService.getBankAccount(id) ?: throw BankAccountNotFoundException("Non-existing account `$id` cannot be blocked.")
         bankAccountService.blockAccount(account)
     }
 
+
     @DeleteMapping("/{id}")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
     fun deleteBankAccount(@PathVariable id: Long) {
         val account = bankAccountService.getBankAccount(id) ?: throw BankAccountNotFoundException("Non-existing account `$id` cannot be deleted.")
         bankAccountService.deleteAccount(account)
--- 1_3_spring_rest/src/main/kotlin/dev/mrthegood/week_three/controller/BankAccountHolderController.kt
+++ 1_3_spring_rest/src/main/kotlin/dev/mrthegood/week_three/controller/BankAccountHolderController.kt
@@ -1,12 +1,12 @@
 package dev.mrthegood.week_three.controller
 
-import dev.mrthegood.week_three.models.BankAccount
 import dev.mrthegood.week_three.models.BankAccountHolder
 import dev.mrthegood.week_three.services.BankAccountHolderService
 import dev.mrthegood.week_three.util.exception.BankAccountHolderNotFoundException
 import dev.mrthegood.week_three.util.exception.IllegalBankAccountHolderException
-import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.net.URI
 
 @RestController
 @RequestMapping("BankAccountHolder")
@@ -15,45 +15,38 @@ class BankAccountHolderController(
 ) {
 
     @GetMapping("")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
     fun getBankAccountHolders() = service.bankAccountHolders
 
+
     @GetMapping("/{id}")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
     fun getBankAccountHolder(@PathVariable id: Long): BankAccountHolder {
         return service.getBankAccountHolder(id) ?: throw BankAccountHolderNotFoundException("Account Holder `$id` not found")
     }
 
+
     @GetMapping("/{holderId}/accounts")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
-    fun getBankBankAccounts(@PathVariable holderId: Long): List<BankAccount> {
-        return service.getBankAccounts(holderId)
-    }
+    fun getBankBankAccounts(@PathVariable holderId: Long) = service.getBankAccounts(holderId)
+
 
     @PostMapping("")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.CREATED)
-    fun createBankAccountHolder(@RequestBody accountHolder: BankAccountHolder): Long {
+    fun createBankAccountHolder(@RequestBody accountHolder: BankAccountHolder): ResponseEntity<Long> {
         if (accountHolder.firstname.isBlank() || accountHolder.lastname.isBlank())
             throw IllegalBankAccountHolderException("Account holder `${accountHolder.id}` cannot be created. Name cannot be blank")
-        return service.createBankAccountHolder(accountHolder)
+
+        val id = service.createBankAccountHolder(accountHolder)
+        return ResponseEntity.created(URI("/$id")).apply { body(id) }.build()
     }
 
+
     @PutMapping("")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
     fun updateBankAccountHolder(@RequestBody accountHolder: BankAccountHolder) {
         if (accountHolder.firstname.isBlank() || accountHolder.lastname.isBlank())
             throw IllegalBankAccountHolderException("Account holder `${accountHolder.id}` cannot be updated. Name cannot be blank")
         service.updateBankAccountHolder(accountHolder)
     }
 
+
     @DeleteMapping("/{id}")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
     fun deleteBankAccountHolder(@PathVariable id: Long) {
         val accountHolder = service.getBankAccountHolder(id) ?: throw BankAccountHolderNotFoundException("Non-existing account holder `$id` cannot be deleted.")
         service.deleteBankAccountHolder(accountHolder)
